1f45c7395185833b28c7a27959113a7c968be412
kishoretaru <tarunkishore017@gmail.com>
2020-07-23 15:45:04 +0530

    first commit

---
+++ src/Config/firebaseConfig.js
@@ -0,0 +1,10 @@
+export default  {
+   apiKey: "AIzaSyCuL4kro9jQvHWV1GPANbQ7UZfrSbjHk5U",
+   authDomain: "my-git-app-1a301.firebaseapp.com",
+   databaseURL: "https://my-git-app-1a301.firebaseio.com",
+   projectId: "my-git-app-1a301",
+   storageBucket: "my-git-app-1a301.appspot.com",
+   messagingSenderId: "663769719340",
+   appId: "1:663769719340:web:d0f8487205dcfcd3b7a789",
+   measurementId: "G-82LYTC2SYP"
+ };
